076c0db68b1bb8fa4cf447eb39cc99bfc8a1aba8
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2024-09-11 20:55:03 +0530

    Time: 87 ms (98.07%), Space: 45 MB (26.02%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -1,5 +1,9 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        Arrays.sort(nums3);
+        Arrays.sort(nums4);
         HashMap<Integer,Integer> hm = new HashMap();
         int count = 0;
         for(int c : nums3)
