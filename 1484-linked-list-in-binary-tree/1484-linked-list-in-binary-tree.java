136953af315f0c71d3e3d1007e425ef159ea4768
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2024-09-16 20:05:31 +0530

    Time: 2 ms (11.54%), Space: 45.2 MB (26.34%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.java
@@ -34,17 +34,14 @@ class Solution {
     }
     public boolean Traversal(int index,List<Integer> ls,TreeNode root){
       if(root == null) return false;
-    //   System.out.print(root.val);
       if(root.val == ls.get(index)) {
         if(checkisSubPath(index,ls,root)) return true;
       }
       return Traversal(index,ls,root.left) || Traversal(index,ls,root.right);
     }
     public boolean checkisSubPath(int index,List<Integer> ls,TreeNode root){
-        // System.out.print(index+"\n");
         if(index == ls.size()) return true;
         if(root == null) return false;
-        // System.out.print(root.val+"->"+"\n");
         if(root.val == ls.get(index)) return checkisSubPath (index+1,ls,root.left) || checkisSubPath(index+1,ls,root.right);
         else return false;  
     }
