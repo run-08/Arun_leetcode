02268146884dff59ce5532241c934e987758b508
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2024-10-08 12:01:01 +0530

    Time: 1 ms (99.44%), Space: 56.6 MB (88.21%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentNum = nums[0];
+        int maximumNum = currentNum;
+        for(int i = 1;i < nums.length;i++) {
+            currentNum += nums[i];
+            if(currentNum < nums[i]) currentNum = nums[i];
+            maximumNum = (currentNum > maximumNum) ? currentNum : maximumNum;      // O( N )
+        }
+        return maximumNum;
+        // int max = arr[0];
+        // for(int i = 0;i < arr.length;i++){
+        //     int currentElement = arr[i];
+        //     if(currentElement > max) max = currentElement;
+        //     for(int j = i+1;j < arr.length;j++) {
+        //             currentElement += arr[j];
+        //             if(max < currentElement) max= currentElement;   // O ( N ^ 2 )
+        //     }
+        // }
+        // return max;
+    }
+}
