35107eebc9598883a6ea671c462aea320201ecd6
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2024-10-22 09:00:08 +0530

    Time: 53 ms (53.46%), Space: 58 MB (73.33%) - LeetHub

--- 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.java
+++ 1988-minimize-maximum-pair-sum-in-array/1988-minimize-maximum-pair-sum-in-array.java
@@ -2,9 +2,7 @@ class Solution {
     public int minPairSum(int[] nums) {
         Arrays.sort(nums);
         int res = 0;
-        for(int i = 0;i < nums.length / 2;i++){
-            if(nums[i]+nums[nums.length-i-1] > res) res = nums[i]+nums[nums.length-i-1] ;
-        }
+        for(int i = 0;i < nums.length / 2;i++) if(nums[i]+nums[nums.length-i-1] > res) res = nums[i]+nums[nums.length-i-1] ;
         return res;
     }
 }
