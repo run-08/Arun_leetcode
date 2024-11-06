a4b4e287be26eff4016096c41f5f09eb10d97fd2
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2024-11-06 23:59:00 +0530

    Time: 1 ms (99.23%), Space: 45 MB (64.12%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-        int jump = 0;
-        int n = nums.length;
+       int jump = 0;
         if(nums.length == 1) return 0;
-        if(nums[0] == 0) return -1;
+         if(nums[0] == 0) return -1;
+        int n = nums.length;
         for(int i = 0;i < n;i++){
             if(nums[i] >= (nums.length - 1) - i) return jump+1;
             jump++;
             int idx = nums[i]+i;
-            int distance = Integer.MIN_VALUE;
+            int distance = nums[i]+i;
             for(int j = i+nums[i];j >= i;j--){
                 int distance_in = nums[j]+j;
-                if(distance_in >= distance) {
+                if(distance_in >= distance){
                     idx = j;
                     distance = distance_in;
                 }
             }
-            i = idx-1 ;
+            i = idx - 1;
         }
         return -1;
+    
     }
 }
