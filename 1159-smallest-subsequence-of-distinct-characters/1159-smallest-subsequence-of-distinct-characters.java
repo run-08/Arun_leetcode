1a0b774522e14602f368acde00b2f74853ef792c
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2024-11-29 17:41:16 +0530

    Time: 5 ms (24.1%), Space: 42.3 MB (5.7%) - LeetHub

---
+++ 1159-smallest-subsequence-of-distinct-characters/1159-smallest-subsequence-of-distinct-characters.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+         Stack<Character> stack = new Stack<>();
+      HashMap<Character , Integer> hm = new HashMap<>();
+      HashSet<Character> hs = new HashSet<>();
+      for(int i = 0;i < s.length();i++) hm.put(s.charAt(i),i);
+      for(int i = 0;i < s.length();i++){
+            if(hs.contains(s.charAt(i))) continue;
+            if(stack.isEmpty() || stack.peek() < s.charAt(i)){
+                stack.push(s.charAt(i));
+                hs.add(s.charAt(i));
+                continue;
+            }
+            while(!stack.isEmpty()){
+                if(stack.peek() < s.charAt(i)){
+                 stack.push(s.charAt(i));
+                  hs.add(s.charAt(i));
+                  break;
+                }
+               else if (hm.get(stack.peek()) > i) hs.remove(stack.pop());
+               else {
+                  stack.push(s.charAt(i));
+                  hs.add(s.charAt(i));
+                  break;
+               }
+            }
+            if(stack.isEmpty()){
+                 stack.push(s.charAt(i));
+                 hs.add(s.charAt(i));
+            }
+        }
+        String res = "";
+        for(Character c : stack) res += String.valueOf(c);
+        return res;
+    }
+}
