20f46e45e50915771af6f4c0252924ba5b37f127
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-01-01 21:05:34 +0530

    Time: 98 ms (47.4%), Space: 96.9 MB (14.24%) - LeetHub

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        HashMap<Integer , List<Integer>> hm = new HashMap<>();
+        for(int arr[] : edges) {
+            List<Integer> ls = hm.getOrDefault(arr[1] , new ArrayList<>());
+            ls.add(arr[0]);
+            hm.put(arr[1] , new ArrayList<>(ls));
+        }
+        HashMap<Integer , HashSet<Integer>> memo = new HashMap<>();
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0;i < n;i++){
+            if(!hm.containsKey(i)) res.add(new ArrayList<>());
+            else {
+                // HashSet<Integer> unique_Ancestors = new HashSet<>();
+                List<Integer> Ancestors = new ArrayList<>();
+                HashSet<Integer> unique_Ancestors = dfs( i , hm , memo);
+                for(int node : unique_Ancestors) Ancestors.add(node);
+                Collections.sort(Ancestors);
+                res.add(Ancestors);
+            } 
+        }
+      
+        return res;
+    }
+   public HashSet<Integer> dfs( int child_node , HashMap<Integer , List<Integer>> hm , HashMap<Integer , HashSet<Integer>> memo){
+        if(memo.containsKey(child_node)) return memo.get(child_node);
+        HashSet<Integer> hs = new HashSet<>();
+        List<Integer> Ancestors = hm.get(child_node);
+        for(int node : Ancestors) {
+            hs.add(node);
+            if(hm.containsKey(node)) hs.addAll(dfs( node , hm , memo)); 
+        }
+         memo.put(child_node ,hs);
+        return hs;
+   }
+}
