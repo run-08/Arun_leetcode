c40a4fed5e6f9195d0362b2cdc29a7f17a50eaaa
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-02-19 14:51:02 +0530

    Time: 23 ms (26.42%), Space: 45.3 MB (28.47%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String getHappyString(int n, int k) {
+        List<String> res = new ArrayList<>();
+        bacres("",res , n);
+        if(res.size() < k) return "";
+        return res.get(k-1);
+    }
+    public void bacres(String s , List<String> res , int n ){
+         if(s.length() == n){
+            // System.out.print(s);
+            res.add(new String(s));
+            return;
+         }
+         int len = s.length();
+        for(char i = 'a' ;i <= 'c';i++)  if(len == 0 || s.charAt(len-1) != i) bacres(s+i , res , n);
+    }
+}
