c50791415227ee441907fb19a810ebb222b5c115
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-06 00:06:10 +0530

    Time: 5 ms (10.47%), Space: 43.6 MB (8.01%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer , List<Integer>> col_contains_rows = new HashMap<>();
+    HashMap<Integer , HashMap<Integer , List<Integer>>> cols = new HashMap<>();
+    int min_depth = 1001;
+    int max_depth = -1001;
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        dfs(root , 0 , 0);
+        List<List<Integer>> result = new ArrayList<>();
+        for(;min_depth <= max_depth;min_depth++){
+            List<Integer> rows = col_contains_rows.get(min_depth);
+            HashMap<Integer , List<Integer>> columns = cols.get(min_depth);
+            List<Integer> res = new ArrayList<>();
+            Collections.sort(rows);
+            for(int row : rows){
+               List<Integer> ls = columns.get(row);
+               Collections.sort(ls);
+               res.addAll(new ArrayList<>(ls));
+            }
+            result.add(new ArrayList<>(res));
+        }
+        return result;
+    }
+    public void dfs(TreeNode root , int row , int col){
+      if(root == null) {
+        min_depth = Math.min(min_depth , col+1);
+        max_depth = Math.max(max_depth , col-1);
+        return;
+      }
+      List<Integer> ls = col_contains_rows.getOrDefault(col , new ArrayList<>());
+      if(!ls.contains(row))ls.add(row);
+      col_contains_rows.put(col , new ArrayList<>(ls));
+      HashMap<Integer , List<Integer>> hm = cols.getOrDefault(col , new HashMap<>());
+      ls = hm.getOrDefault(row , new ArrayList<>());
+      ls.add(root.val);
+      hm.put(row , new ArrayList<>(ls));
+      cols.put(col , new HashMap<>(hm));
+      dfs(root.left , row+1 , col-1);
+      dfs(root.right , row+1 , col+1);
+    }
+}
