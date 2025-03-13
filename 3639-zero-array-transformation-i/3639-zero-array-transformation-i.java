1ed8e373690e1ba2885baf75cb64c92424dcf573
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-13 18:08:34 +0530

    Time: 3 ms (80.91%), Space: 96.4 MB (5.99%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        HashMap<Integer , int[][]> hm = new HashMap<>();
-        for(int[] arr : queries) {
-            int[][] freq = hm.getOrDefault(arr[0] , new int[1][2]);
-            freq[0][0]++;
-            hm.put(arr[0] , freq.clone());
-            freq = hm.getOrDefault(arr[1] , new int[1][2]);
-            freq[0][1]++;
-            hm.put(arr[1] , freq.clone());
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int[] arr : queries){
+            int start = arr[0];
+            int end = arr[1];
+            ans[start] -= 1;
+            if(end+1 < n) ans[end+1] += 1;
         }
-        int differer = 0 ,  n = nums.length;
-        for(int i = 0;i < n;i++){
-            if(hm.containsKey(i)) differer += hm.get(i)[0][0];
-            nums[i] -= differer;
-            if(hm.containsKey(i)) differer -= hm.get(i)[0][1];
+        for(int i = 1;i < n;i++) ans[i] += ans[i-1];
+        for(int i = 0;i < n;i++) {
+            nums[i] += ans[i];
+            if(nums[i] > 0) return false;
         }
-        for(int  val : nums) if(val > 0) return false;
         return true;
     }
 }
