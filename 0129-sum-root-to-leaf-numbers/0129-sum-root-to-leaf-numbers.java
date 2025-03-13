025e5c151c44aee5f018fd4c96ec528f4a28ceae
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-13 22:47:09 +0530

    Time: 0 ms (100%), Space: 41.3 MB (52.7%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -17,15 +17,15 @@ class Solution {
     int res ;
     public int sumNumbers(TreeNode root) {
          res = 0;
-         dfs(root , "");
+         dfs(root , 0);
          return res;
     }
-    public void dfs(TreeNode root , String sum ){
+    public void dfs(TreeNode root , int sum ){
         if(root.left == null && root.right == null) {
-            res += Integer.parseInt(sum+String.valueOf(root.val));
+            res += (sum * 10) + root.val;
             return;
         }
-        if(root.left != null) dfs(root.left,sum+String.valueOf(root.val));
-        if(root.right != null) dfs(root.right , sum+String.valueOf(root.val));
+        if(root.left != null) dfs(root.left, (sum * 10)+root.val);
+        if(root.right != null) dfs(root.right , (sum * 10)+root.val);
     }
 }
