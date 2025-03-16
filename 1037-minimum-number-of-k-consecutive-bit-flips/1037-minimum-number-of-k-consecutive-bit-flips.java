aaa72cebee8fb3ebe3aa9748127751f837d39f3a
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-16 19:58:41 +0530

    Time: 22 ms (7.14%), Space: 57.2 MB (25.63%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
@@ -4,7 +4,7 @@ class Solution {
         int res = 0 , n = nums.length , cnt = 0 ;
         for(int i = 0;i < n;i++) {
             if(hs.contains(i)) cnt-= 1;
-            nums[i] ^= ((cnt & 1) == 0) ? 0 : 1; 
+            nums[i] ^= (cnt & 1); 
             if(nums[i] == 1) continue;
             if((i+k) > n) return -1;
             else {
