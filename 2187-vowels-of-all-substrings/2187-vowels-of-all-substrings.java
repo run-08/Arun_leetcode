71c4f79ac154d8084b1e8c67cc6d78cf9304c3d1
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-18 18:59:55 +0530

    Time: 8 ms (93.71%), Space: 45.1 MB (96.86%) - LeetHub

--- 2187-vowels-of-all-substrings/2187-vowels-of-all-substrings.java
+++ 2187-vowels-of-all-substrings/2187-vowels-of-all-substrings.java
@@ -3,7 +3,8 @@ class Solution {
         long res = 0 , p_sum = 0;
         int n = word.length();
         for(int i = 0;i < n;i++){
-            if(word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u')  p_sum += i+1;
+            char c = word.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')  p_sum += i+1;
             res += p_sum;
         }
         return res;
