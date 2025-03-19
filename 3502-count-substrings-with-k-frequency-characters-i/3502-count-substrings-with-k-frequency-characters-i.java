1c0e6762388e7c49076c4a783840a72692fe907a
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-19 14:31:52 +0530

    Time: 6 ms (38.4%), Space: 44.5 MB (27.6%) - LeetHub

--- 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.java
+++ 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.java
@@ -1,6 +1,6 @@
 class Solution {
     public int numberOfSubstrings(String s, int k) {
-        HashMap<Character , List<Integer>> hm = new HashMap<>();
+        HashMap<Character , Deque<Integer>> hm = new HashMap<>();
         boolean[] freq = new boolean[26];
         int res = 0 , max = 0 , n = s.length();
         for(int i = 0;i < n;i++) {
@@ -8,19 +8,19 @@ class Solution {
             int idx = c-97;
             if(hm.containsKey(c)) hm.get(c).add(i);
             else {
-                List<Integer> ls = new ArrayList<>();
+                Deque<Integer> ls = new LinkedList<>();
                 ls.add(i);
-                hm.put(c , new ArrayList<>(ls));
+                hm.put(c , new LinkedList<>(ls));
             }
             if(freq[idx]) {
-                hm.get(c).remove(0);
-                max = Math.max(hm.get(c).get(0)+1 , max);
+                hm.get(c).pollFirst();
+                max = Math.max(hm.get(c).peekFirst()+1 , max);
             }
             else {
                 int size = hm.get(c).size();
                 if(size == k) {
                    freq[idx] = true;
-                   max = Math.max(hm.get(c).get(0)+1 , max);
+                   max = Math.max(hm.get(c).peekFirst()+1 , max);
                 }
             }
             res += max;
