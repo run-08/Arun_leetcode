0c76bf45c82933331e3a61a293509cb0167ed656
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-19 23:19:10 +0530

    Time: 2 ms (100%), Space: 61.5 MB (10.94%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,24 +1,52 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long res = 0 , sum = 0;
-        int idx = 0 , n = nums.length;
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i = 0;i < n;i++) {
-            sum += nums[i];
-            if(hs.contains(nums[i])) {
-               while(hs.contains(nums[i])) {
-                hs.remove(nums[idx]);
-                sum -= nums[idx++];
-               }
-               hs.add(nums[i]);
-            }
-            else hs.add(nums[i]);
-            if(hs.size() >= k) {
-                res = Math.max(sum , res);
-                sum -= nums[idx];
-                hs.remove(nums[idx++]);
+        int max = 0;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
             }
         }
-        return res;
+
+        int[] count = new int[max + 1];
+        int numNonOne = 0;
+        long sum = 0;
+        long totalSum = 0;
+        for (int i = 0; i < k; i++) {
+            int num = nums[i];
+            sum += num;
+            if (count[num] == 1) {
+                numNonOne++;
+            }
+            //System.out.println("num " + num + ", sum" + sum + ", n1Flag " + numNonOne);
+            count[num]++;
+            //System.out.println(Arrays.toString(count));
+        }
+
+        if (numNonOne == 0 && sum > totalSum) {
+            totalSum = sum;
+        }
+
+        int n = nums.length;
+        for (int i = k; i < n; i++) {
+            int num = nums[i];
+            int prevNum = nums[i - k];
+            sum += num;
+            sum -= prevNum;
+            //System.out.println("prev Num" + prevNum + "num " + num + ", sum" + sum + ", n1Flag " + numNonOne);
+            if (count[prevNum] == 2) {
+                numNonOne--;
+            }
+            count[prevNum]--;
+            if (count[num] == 1) {
+                numNonOne++;
+            }
+            count[num]++;
+            //System.out.println(Arrays.toString(count));
+            if (numNonOne == 0 && sum > totalSum) {
+                totalSum = sum;
+            }
+        }
+
+        return totalSum; 
     }
 }
