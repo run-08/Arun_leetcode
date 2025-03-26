91b01e14a3804b9d73449d599f17fea535eaa581
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-26 08:35:36 +0530

    Time: 205 ms (7.54%), Space: 71.3 MB (73.87%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int n = grid.length , res = 0;
+        List<Integer> ls = new ArrayList<>();
+        for(int[] row : grid) {
+            for(int val : row) ls.add(val);
+        }
+        Collections.sort(ls);
+        System.out.print(ls);
+        int mid_value = ls.get( ls.size() / 2 );
+        for(int val : ls ){
+           int complement = Math.abs(mid_value-val);
+           if(complement % x != 0) return -1;
+           res += complement / x;
+        }
+        return res;
+    }
+}
