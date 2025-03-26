d6e051e60cd7b4fa1b3ef7ab2f86a3bf2003c630
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-26 14:18:54 +0530

    Time: 27 ms (52.07%), Space: 62.7 MB (90.64%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-         int s = 1 , e = 0 , max = 0;
+         int s = 1 , e = 0 ;
          for(int val : candies) e = Math.max(val ,e);
          while(s <= e) {
-            // System.out.print(s+" "+e+"\n");
             int m = s + (e - s ) / 2;
             long cnt = 0;
             for(long val : candies) cnt += val / m;
-            if(cnt >= k) {
-                max = Math.max(max , m);
-                s = m + 1;
-            }
+            if(cnt >= k) s = m + 1;
             else e = m - 1;
          }
-         return max;
+         return e;
     }
 }
