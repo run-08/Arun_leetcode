0c3a4de16fb81203b3425032cdadc3e8c8c9b8e6
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-27 08:18:12 +0530

    Time: 37 ms (39.1%), Space: 57.3 MB (67.31%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumIndex(List<Integer> nums) {
+        int max = 0 , n = nums.size() , cnt = 0 , high = 0;
+        HashMap<Integer , Integer> freq = new HashMap<>();
+        for(int i = 0;i < n;i++) {
+            freq.put(nums.get(i)  , freq.getOrDefault(nums.get(i) , 0)+1);
+            if(freq.get(nums.get(i)) > max) {
+                max = freq.get(nums.get(i));
+                high = nums.get(i);
+            }
+        }
+        int idx = 0;
+        for(;idx < n ;idx++) {
+            if((cnt > (idx-cnt))) break;
+            if(nums.get(idx) == high) cnt++; 
+        } 
+        // System.out.print(idx);
+        cnt = 0;
+        for(int i = idx;i < n;i++) if(high == nums.get(i)) cnt++;
+        int target = n-idx;
+        if(target-cnt >= cnt) return -1;
+        return idx-1;
+    }
+}
