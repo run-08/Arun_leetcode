8e4694b6e90e4877901a9571b750c520d8f1e276
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-03-27 22:28:54 +0530

    Time: 13 ms (74.81%), Space: 44.8 MB (44.22%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,20 +1,16 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int count = 0;
-        int max = 0;
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0;i < k;i++){
-         if(s.charAt(i) == 'a' || s.charAt(i) == 'A' || s.charAt(i) == 'e' || s.charAt(i) == 'E' || s.charAt(i) == 'i' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'o' || s.charAt(i) == 'U' || s.charAt(i) == 'u') count++;
-      sb.append(String.valueOf(s.charAt(i)));
-        }
-        max = count;
-        for(int i = k;i < s.length();i++){
-            sb.append(String.valueOf(s.charAt(i)));
-         if(s.charAt(i) == 'a' || s.charAt(i) == 'A' || s.charAt(i) == 'e' || s.charAt(i) == 'E' || s.charAt(i) == 'i' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'o' || s.charAt(i) == 'U' || s.charAt(i) == 'u') count++;
-              if(sb.charAt(0) == 'a' || sb.charAt(0) == 'A' || sb.charAt(0) == 'e' || sb.charAt(0) == 'E' || sb.charAt(0) == 'i' || sb.charAt(0) == 'I' || sb.charAt(0) == 'O' || sb.charAt(0) == 'o' || sb.charAt(0) == 'U' || sb.charAt(0) == 'u') count--;
-              sb.delete(0,1);
-              if(count > max) max = count;
+        int n = s.length() , cnt = 0 , max = 0;
+        for(int i = 0;i < n;i++) {
+            char c = s.charAt(i);
+            if(isVowel(c))cnt++;
+            if(i+1 < k) continue;
+            max = Math.max(max , cnt);
+            if(isVowel(s.charAt((i+1)-k))) cnt--;
         }
         return max;
+    }
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
