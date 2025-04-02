4f99f92eb29b30d48ea43f8b05902230d38a8373
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-04-02 15:52:10 +0530

    Time: 275 ms (5.53%), Space: 70.7 MB (98.59%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n = s.length();
+        int[] p_sum = new int[n];
+        for(int[] arr : shifts) {
+            int start = arr[0];
+            int end = arr[1];
+            int value = (( arr[2] & 1 ) == 0 ) ? -1 : 1;
+            p_sum[start] += value;
+            if(value == -1) value = 1;
+            else value = -1;
+            if(end+1 < n) p_sum[end+1] += value;
+        }
+        // System.out.print(Arrays.toString(p_sum));
+        int sum = 0;
+        String res = "";
+        for(int i = 0;i < n;i++) {
+            sum += p_sum[i];
+            boolean isNegative = sum < 0;
+            sum = Math.abs(sum);
+            // System.out.print(sum+" ");
+            if(sum == 0) {
+                res += s.charAt(i);
+                continue;
+            }
+            int val = sum % 26;
+            int ascii_val = s.charAt(i);
+            if(!isNegative) {
+                  ascii_val += val;
+                 if(ascii_val > 122) {
+                    ascii_val -= 122;
+                    ascii_val += 96;
+                 }
+            }
+            else {
+                sum = -sum;
+                ascii_val -= val;
+                if(ascii_val < 97) {
+                    ascii_val = 97 - ascii_val;
+                    ascii_val = 123 - ascii_val;
+                }
+            }
+            res += (char)(ascii_val);
+        }
+        return res;
+    }
+}
