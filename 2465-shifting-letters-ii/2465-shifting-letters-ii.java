4859d2f65e6aeb39c83abb796c6490f91b4c12cc
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-04-02 15:55:00 +0530

    Time: 10 ms (15.18%), Space: 74.8 MB (34.27%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.java
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -12,13 +12,13 @@ class Solution {
             if(end+1 < n) p_sum[end+1] += value;
         }
         int sum = 0;
-        String res = "";
+        StringBuilder res = new StringBuilder();
         for(int i = 0;i < n;i++) {
             sum += p_sum[i];
             boolean isNegative = sum < 0;
             sum = Math.abs(sum);
             if(sum == 0) {
-                res += s.charAt(i);
+                res.append(s.charAt(i));
                 continue;
             }
             int val = sum % 26;
@@ -38,8 +38,8 @@ class Solution {
                     ascii_val = 123 - ascii_val;
                 }
             }
-            res += (char)(ascii_val);
+            res.append((char)(ascii_val));
         }
-        return res;
+        return res.toString();
     }
 }
