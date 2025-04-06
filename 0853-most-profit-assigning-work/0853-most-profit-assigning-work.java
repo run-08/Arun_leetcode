2c59535028652ca577c7d0859f1319ee72c8629f
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-04-06 17:29:04 +0530

    Time: 23 ms (52.84%), Space: 46 MB (42.09%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        HashMap<Integer , Integer> hm = new HashMap<>();
+        int n = difficulty.length , res = 0;
+        for(int i = 0;i < n;i++) hm.put(difficulty[i] , Math.max( hm.getOrDefault(difficulty[i] , 0) , profit[i]));
+        Arrays.sort(difficulty);
+        profit[0] = hm.get(difficulty[0]);
+        for(int i = 1;i < n;i++) profit[i] = Math.max(profit[i-1] , hm.get(difficulty[i]));
+        for(int target : worker) {
+            int s = 0 , e = n - 1;
+            while(s <= e) {
+                int  m = s + ( e - s ) / 2;
+                if(difficulty[m] == target) {
+                    e = m;
+                    break;
+                }
+                else if(difficulty[m] > target) e = m - 1;
+                else s = m + 1;
+            }
+            if(e >= 0) res += (profit[e]);
+        }
+        return res;
+    }
+}
