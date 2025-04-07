cf0f0f7b426446a750ebfeb9f4824e2f77340a1e
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-04-07 16:12:46 +0530

    Time: 121 ms (31.46%), Space: 86.1 MB (46.52%) - LeetHub

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int amountOfTime(TreeNode root, int start) {
+        HashMap<Integer , Queue<Integer>> hm = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            Queue<TreeNode> child = new LinkedList<>();
+            while(!q.isEmpty()) {
+                TreeNode node = q.poll();
+                 if(node.left != null) {
+                    hm.computeIfAbsent(node.val , k-> new LinkedList<>()).offer(node.left.val);
+                    hm.computeIfAbsent(node.left.val , k -> new LinkedList<>()).offer(node.val);
+                    child.offer(node.left);
+                 }
+                 if(node.right != null) {
+                    hm.computeIfAbsent(node.val , k -> new LinkedList<>()).offer(node.right.val);
+                    hm.computeIfAbsent(node.right.val , k -> new LinkedList<>()).offer(node.val);
+                    child.offer(node.right);
+                 }
+            }
+            if(child.size() > 0) q = child;
+        }
+       if(!hm.containsKey(start)) return 0;
+       Queue<Integer> qq = hm.get(start);
+       HashSet<Integer> hs = new HashSet<>();
+       int cnt = -1;
+       hs.add(start);
+       while(!qq.isEmpty()) {
+           Queue<Integer> child = new LinkedList<>();
+           cnt += 1;
+           while(!qq.isEmpty()) {
+                Queue<Integer> parentChild = hm.get(qq.poll());
+                while(!parentChild.isEmpty()) {
+                    int node = parentChild.poll();
+                    if(hs.contains(node)) continue;
+                    child.offer(node);
+                }
+           }
+           if(child.size() > 0) qq = child;
+       }
+       return cnt > 1 ?  cnt : 1;
+    }
+}
