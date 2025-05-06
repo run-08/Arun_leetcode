62ce5ca949a71ec6d94f5de741f0f80d3de90924
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-05-06 17:43:20 +0530

    Time: 1718 ms (5.59%), Space: 46.7 MB (22.36%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.java
@@ -1,60 +1,49 @@
 class Solution {
+    Stack<Character> stack = new Stack<>();
     public int maximumGain(String s, int x, int y) {
         int sum = 0;
-        if (x > y) {
-            StringBuilder sb = new StringBuilder();
-            sum = removeAB(s, x, sb);  // Remove "ab" first
-            sum += removeBA(sb.toString(), y);  // Then remove "ba"
-        } else {
-            StringBuilder sb = new StringBuilder();
-            sum = removeBA(s, y, sb);  // Remove "ba" first
-            sum += removeAB(sb.toString(), x);  // Then remove "ab"
-        }
-        return sum;
+       if(x > y ) {
+        sum = X( s , x , y ) ;
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) sb.insert(0 , stack.pop());
+        sum += Y( sb.toString() , x , y );
+       }
+       else  {
+        sum = Y( s , x , y ); 
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) sb.insert(0 , stack.pop());
+        sum += X( sb.toString() , x , y );
+       }
+       return sum;
     }
-
-    // Remove "ab" worth x points
-    private int removeAB(String s, int x, StringBuilder leftover) {
-        Stack<Character> stack = new Stack<>();
+    // for x
+    public int X(String s , int x , int y){
         int sum = 0;
-        for (char c : s.toCharArray()) {
-            if (c == 'b' && !stack.isEmpty() && stack.peek() == 'a') {
-                stack.pop(); // Remove 'a'
-                sum += x;
-            } else {
-                stack.push(c);
+        for(char c : s.toCharArray()) {
+            if(c == 'b') {
+                if(!stack.isEmpty() && stack.peek() == 'a') {
+                    stack.pop();
+                    sum += x;
+                }
+                else stack.push(c);
             }
-        }
-        while (!stack.isEmpty()) {
-            leftover.insert(0, stack.pop());
+            else stack.push(c);
         }
         return sum;
     }
-
-    // Remove "ba" worth y points
-    private int removeBA(String s, int y, StringBuilder leftover) {
-        Stack<Character> stack = new Stack<>();
+    // for Y
+    public int Y(String s , int x , int y){
         int sum = 0;
-        for (char c : s.toCharArray()) {
-            if (c == 'a' && !stack.isEmpty() && stack.peek() == 'b') {
-                stack.pop(); // Remove 'b'
-                sum += y;
-            } else {
-                stack.push(c);
+      for(char c : s.toCharArray()) {
+            if(c == 'a') {
+                if(!stack.isEmpty() && stack.peek() == 'b') {
+                    stack.pop();
+                    sum += y;
+                }
+                else stack.push(c);
             }
-        }
-        while (!stack.isEmpty()) {
-            leftover.insert(0, stack.pop());
+            else stack.push(c);
         }
         return sum;
-    }
-
-    // Overloaded version when leftover isn't needed
-    private int removeBA(String s, int y) {
-        return removeBA(s, y, new StringBuilder());
-    }
-
-    private int removeAB(String s, int x) {
-        return removeAB(s, x, new StringBuilder());
     }
 }
