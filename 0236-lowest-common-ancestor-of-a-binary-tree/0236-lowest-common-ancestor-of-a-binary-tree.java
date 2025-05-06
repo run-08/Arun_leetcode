84b1288323accc75d1191e6f307a6178cdc8b740
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-05-07 00:16:59 +0530

    Time: 13 ms (9.11%), Space: 46.4 MB (5.47%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,32 +1,45 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        HashMap<TreeNode ,  TreeNode> hm = new HashMap<>();
-        hm.put(root , null);
-        dfs(root , hm);
-        List<TreeNode> ls = new ArrayList<>();
-        TreeNode s = p;
-        while(s != null) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(root.val, null);
+        dfs(root, hm);
+
+        List<Integer> ls = new ArrayList<>();
+        int s = p.val;
+        while (true) {
             ls.add(s);
+            if (hm.get(s) == null) break;
             s = hm.get(s);
         }
-        s = q;
-        while(!ls.contains(s)) s = hm.get(s);
-        return s;
+
+        int target;
+        s = q.val;
+        while (true) {
+            if (ls.contains(s)) {
+                target = s;
+                break;
+            }
+            s = hm.get(s);
+        }
+
+        return findTarget(target, root);
     }
-     public void dfs(TreeNode root , HashMap<TreeNode , TreeNode> hm){
-        if(root == null) return;
-        if(root.left != null) hm.put(root.left , root);
-        if(root.right != null) hm.put(root.right , root);
-        dfs(root.left , hm);
-        dfs(root.right , hm);
-     }
+
+    public void dfs(TreeNode root, HashMap<Integer, Integer> hm) {
+        if (root == null) return;
+        if (root.left != null) hm.put(root.left.val, root.val);
+        if (root.right != null) hm.put(root.right.val, root.val);
+        dfs(root.left, hm);
+        dfs(root.right, hm);
+    }
+
+    public TreeNode findTarget(int target, TreeNode root) {
+        if (root == null) return null;
+        if (root.val == target) return root;
+
+        TreeNode left = findTarget(target, root.left);
+        if (left != null) return left;
+
+        return findTarget(target, root.right);
+    }
 }
