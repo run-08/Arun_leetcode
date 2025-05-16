56771ff443456c9712fc80522e22dd90353f50d6
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-05-16 19:11:13 +0530

    Time: 0 ms (100%), Space: 41.4 MB (15.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,10 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length < 2) return nums[0];
-        int[] dp = new int[nums.length];
-         dp[0] = nums[0];
-         dp[1] = (nums[0] < nums[1]) ? nums[1] : nums[0];
-        for(int i = 2;i < nums.length;i++) dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
-        return dp[nums.length-1];
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp ,-1);
+        return memo_f_robber(nums , dp , n-1);
     }
+//  private static int rec_f_robber(int[] arr , int idx){
+//     if(idx < 0) return 0;
+//     if(idx == 0) return arr[0];
+//    return Math.max( (arr[idx]+rec_f_robber(arr , idx-2)) , (rec_f_robber(arr , idx-1)) );
+// }
+private static int memo_f_robber(int[] arr , int[] dp , int idx){
+    if(idx < 0) return 0;
+    if(idx == 0) return arr[0];
+    if(dp[idx] != -1) return dp[idx];
+    return dp[idx] = Math.max( arr[idx]+memo_f_robber(arr, dp , idx-2) , (memo_f_robber(arr,dp , idx-1)) );
+  }
 }
