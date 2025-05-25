178f5366ec1e547c42423fbc40256d0dc90254d9
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-05-25 08:53:38 +0530

    Time: 65 ms (52.44%), Space: 59.5 MB (29.06%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,25 +1,39 @@
 class Solution {
     public int longestPalindrome(String[] words) {
+        int res = 0;
         HashMap<String , Integer> hm = new HashMap<>();
         for(String word : words) hm.put(word , hm.getOrDefault(word,0)+1);
-        int res = 0;
-        boolean isMid = false;
-        for(String word : words) {
+        boolean isPair = false;
+        for(String word : words){
             if(!hm.containsKey(word)) continue;
-            if(word.charAt(0) == word.charAt(1)) {
-               int one = hm.get(word);
-               if((one & 1) == 1) if(isMid) one -= 1;
-               else isMid = true;
-               res += one * 2;
-               hm.remove(word);
-               continue; 
+            if(word.charAt(0) == word.charAt(1) ) {
+                int freq = hm.get(word);
+                if(freq == 1 ) {
+                    if(isPair) continue;
+                    res += 2;
+                    isPair = true;
+                }
+                else {
+                    if(freq % 2 == 0) res += freq * 2;
+                    else {
+                        res += ( freq - 1) * 2;
+                        if(!isPair) {
+                            isPair = true;
+                            res += 2;
+                        }
+                    }
+                }
+                hm.remove(word);
             }
-            String word_rev = word.charAt(1)+""+word.charAt(0);
-            if(!hm.containsKey(word_rev)) continue;
-            int one = hm.get(word) , two = hm.get(word_rev);
-            res += Math.min(one , two) * 4;
+            String rev = word.charAt(1)+""+word.charAt(0);
+            if(!hm.containsKey(rev)) {
+                hm.remove(word);
+                continue;
+            }
+            int min = Math.min(hm.get(word)  , hm.get(rev));
             hm.remove(word);
-            hm.remove(word_rev);
+            hm.remove(rev);
+            res += min * 4;
         }
         return res;
     }
