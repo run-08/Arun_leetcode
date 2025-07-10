ded3f487389ec313c155d8bdec41354276b1a278
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-07-10 21:10:44 +0530

    Time: 13 ms (69.94%), Space: 51.6 MB (68.02%) - LeetHub

--- 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.java
+++ 1120-flower-planting-with-no-adjacent/1120-flower-planting-with-no-adjacent.java
@@ -6,33 +6,16 @@ class Solution {
             edges.get ( path [ 0 ] ).add ( path [ 1 ] );
             edges.get ( path [ 1 ] ).add ( path [ 0 ] );
         }
-        Queue < Integer > q = new ArrayDeque <> ();
         int [] coloredNodes = new int [ n  ];
         for ( int vertex =  1 ; vertex <= n ; vertex++ ){
-            if ( coloredNodes [ vertex - 1] != 0 ) continue;
-            q.offer ( vertex );
-            while  ( !q.isEmpty() ) {
-                int node = q.poll();
-                List < Integer > children = edges.get ( node );
+                if ( coloredNodes [ vertex - 1] != 0 ) continue;
+                List < Integer > children = edges.get ( vertex );
                 boolean [] flowerType = new boolean [ 5 ];
                 for ( int child : children ) flowerType [ coloredNodes [ child - 1 ] ] = true;
                 for ( int color = 1 ; color  <= 4 ; color++ ) if ( !flowerType [ color ] ) {
-                    coloredNodes [ node - 1 ] = color;
+                    coloredNodes [ vertex - 1 ] = color;
                     break;
                 }
-                // for ( int child : children ) {
-                //     if ( flowerType [ coloredNodes [ child - 1 ] ] )  continue;
-                //     else {
-                //         for ( int color = 1 ; color <= 4 ; color++ ) {
-                //             if ( !flowerType [ color ] )  {
-                //                 flowerType [ coloredNodes [ child - 1 ] ]  = true;
-                //                 coloredNodes [ child - 1 ] = color; 
-                //             }
-                //             q.offer ( child );
-                //         }
-                //     }
-                // }
-            }
         }
         return coloredNodes;
     }
