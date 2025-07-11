e8a2aa2f620d5b57246f97de6a2ef54757476049
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-07-11 21:01:07 +0530

    Time: 0 ms (100%), Space: 45.2 MB (19.37%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.java
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.java
@@ -15,25 +15,25 @@
  */
 class Solution {
     List < TreeNode > Forest;
-    List < Integer > neededToDelete = new ArrayList <> ();
+    boolean [] neededToDelete;
     public List< TreeNode > delNodes ( TreeNode root, int[] to_delete ) {
         Forest = new ArrayList <> ();
-        for ( int tree : to_delete ) neededToDelete.add ( tree );
+        neededToDelete = new boolean [ 1001 ];
+        for ( int tree : to_delete ) neededToDelete [ tree ] = true;
         if ( root == null ) return Forest;
         dfs ( root );
-        if ( !neededToDelete.contains ( root.val ) ) Forest.add ( root );
+        if ( !neededToDelete [ root.val ] ) Forest.add ( root );
         return Forest;
     }
     private TreeNode dfs ( TreeNode root ) {
-        
-        if ( root == null ||  ( neededToDelete.contains ( root.val) && root.right == null && root.left == null ) ) return null;
-       root.left = dfs ( root.left );
-       root.right = dfs ( root.right );
-       if (neededToDelete.contains ( root.val ) ) {
-         if ( root.left != null ) Forest.add( root.left );
-         if ( root.right != null ) Forest.add( root.right );
-         return null;
-       }
+        if ( root == null ) return root;
+        root.left = dfs ( root.left );
+        root.right = dfs ( root.right );
+        if (neededToDelete [ root.val ] ) {
+          if ( root.left != null ) Forest.add( root.left );
+          if ( root.right != null ) Forest.add( root.right );
+          return null;
+        }
         return root;
     }
 }
