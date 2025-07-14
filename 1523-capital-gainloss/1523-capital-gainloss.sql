76203e63e7782ea412eb7f432e7d92b1c1427c14
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-07-14 12:06:06 +0530

    Time: 600 ms (16.13%), Space: 0B (100%) - LeetHub

---
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select  stock_name , SUM( case when operation Like 'S%' then price else 0 end  ) - SUM( case when operation Like 'B%' then price else 0 end ) as capital_gain_loss  from Stocks  group by stock_name 
