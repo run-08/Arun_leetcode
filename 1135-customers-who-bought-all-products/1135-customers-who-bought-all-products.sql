ad7a5aa1399dca614a04d168403ace39f0d48bb4
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-07-14 08:26:16 +0530

    Time: 763 ms (8.61%), Space: 0B (100%) - LeetHub

---
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select c.customer_id  from Customer c  group by c.customer_id having count( distinct c.product_key) =  ( select count( *  ) from Product p ) ;
