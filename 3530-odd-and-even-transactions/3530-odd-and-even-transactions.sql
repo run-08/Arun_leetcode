f75bb611ddb1638eceb400fc3f0a2a19d288525a
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-07-15 09:28:34 +0530

    Time: 254 ms (96.44%), Space: 0B (100%) - LeetHub

---
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select transaction_date , SUM( if(amount % 2 != 0 , amount , 0 )) as odd_sum , SUM( if(amount % 2 = 0 , amount  , 0 )) as even_sum from transactions group by transaction_date order by transaction_date asc;
