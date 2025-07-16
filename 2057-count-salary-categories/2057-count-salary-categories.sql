d4b7babbeb641f97e8a9afdb36be6cf9cb3fef81
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-07-16 19:23:36 +0530

    Time: 1483 ms (74%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,1 +1,18 @@
 # Write your MySQL query statement below
+with cte_as as (
+     select sum(if(income > 50000 , 1 , 0)) as 'accounts_count' from Accounts
+     union all
+     select sum(if(income < 20000 , 1 , 0))  from Accounts
+     union all
+     select sum(if(income >= 20000 and income <= 50000 , 1 , 0)) from Accounts
+),
+cte_name as (
+    select 'High Salary' as category union select 'Low Salary' union select 'Average Salary'
+),
+numbered_salary as (
+    select accounts_count , row_number() over() as rn from cte_as
+),
+numbered_name as (
+    select category , row_number() over() as rn from cte_name 
+)
+select numbered_name.category , numbered_salary.accounts_count from numbered_name inner join numbered_salary on numbered_salary.rn = numbered_name.rn;
