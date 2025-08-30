bbeffcb2c46385e7cf6970c7e502298ac2120ec0
Arun Thangavel <137892849+run-08@users.noreply.github.com>
2025-08-30 23:24:08 +0530

    Time: 2 ms (77.42%), Space: 44.7 MB (48.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,31 +1,38 @@
 class Solution {
     public boolean isValidSudoku(char[][] nums) {
+        
         HashSet<Character> set = new HashSet<>();
-        for(int i = 0;i < nums.length;i += 3){
-            for(int j = 0;j < nums[0].length;j++){
-                if(set.contains(nums[i][j]) || set.contains(nums[i+1][j]) || set.contains(nums[i+2][j])) return false;
-                if(nums[i][j] != '.' && nums[i][j] != ',')set.add(nums[i][j]);
-                if(nums[i+1][j] != '.' &&  nums[i+1][j] != ',')set.add(nums[i+1][j]);
-                if(nums[i+2][j] != '.' && nums[i+2][j] != ',')set.add(nums[i+2][j]);
-                if((j+1) % 3 == 0) set.clear();
+
+        for(int i = 0;i < 9;i += 3){
+            for(int j = 0;j < 9;j++){
+               
+                if(nums[i][j] != '.' && nums[i][j] != ',')if(!set.add(nums[i][j])) return false;
+                if(nums[i+1][j] != '.' &&  nums[i+1][j] != ',')if(!set.add(nums[i+1][j])) return false;
+                if(nums[i+2][j] != '.' && nums[i+2][j] != ',')if(!set.add(nums[i+2][j])) return false;
+                if((j+1) % 3 == 0) set = new HashSet <> ();
             }
         }
-        for(int i = 0;i < nums.length;i++){
-            for(int j = 0;j < nums[0].length;j++){
+
+        for(int i = 0;i < 9;i++){
+            for(int j = 0;j < 9;j++){
                 if(nums[i][j] == '.' || nums[i][j] == ',') continue;
                 if(set.contains(nums[i][j])) return false;
                 set.add(nums[i][j]);
             }
-            set.clear();
+            set = new HashSet <> ();
         }
-         for(int i = 0;i < nums[0].length;i++){
-            for(int j = 0;j < nums.length;j++){
+
+
+         for(int i = 0;i < 9;i++){
+            for(int j = 0;j < 9;j++){
                   if(nums[j][i] == '.' || nums[j][i] == ',') continue;
                 if(set.contains(nums[j][i])) return false;
                 set.add(nums[j][i]);
             }
-            set.clear();
+            set = new HashSet <> ();
         }
+
+
         return true;
     }
 }
