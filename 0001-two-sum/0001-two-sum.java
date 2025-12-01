8138a2a525c18799659b2f85bff280502a342424
Arun T <137892849+run-08@users.noreply.github.com>
2025-12-01 21:59:06 +0530

    Time: 40 ms (35.14%), Space: 45.2 MB (57.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,50 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer>map = new HashMap<>();
-        int complement,currentNumber;
-        int[] resultindex = new int[2];
-        for(int i =0;i<nums.length;i++){
-              currentNumber = nums[i];
-              complement = target-currentNumber;
-              if(map.containsKey(complement)){
-                  resultindex[0] = i;
-                  resultindex[1] = map.get(complement);
-                  return resultindex;
-              }
-          else{
-            map.put(currentNumber,i);
-          }        
-       }
-       return new int[]{-1, -1};
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{}; // No solution found
     }
 }
-/*
-class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        int rowSize = mat.length , colSize = mat [ 0 ].length;
-        int [] [] dp = new int [ rowSize ] [ colSize ];
-        for ( int [] arr : dp ) Arrays.fil( arr , Integer.MAX_VALUE - 1  );
-        for ( int index = 0 ; index < rowSize ; index++ ) {
-            for ( int index2 = 0 ; index2 < colSize ; index2++ ) {
-                if ( grid [ index ] [ index2 ] == 0 ) {
-                    dp [ index ] [ index1 ] = 0;
-                    continue;
-                }
-                Queue < int []  > q = new ArrayList <> ();
-                q.offer ( new int [] { index , index1 , 0 } );
-                while ( !q.isEmpty() ) {
-                    int [] adjacent = q.pol();
-                    int row = adjacent [ 0 ] , col = adjacent [ 1 ] , steps = adjacent [ 2 ];
-                    if ( dp [ index ] [ index1 ] != Integer.MAX_VALUE ) {
-                        dp [ index ] [ index1 ] = steps;
-                        break;
-                    }
-                    int minDistance = Integer.MAX_VALUE  selectedRow = row , selectedColumn = 
-                    if ( row + 1 < rowsize && minDistance >= dp [ row + 1 ] [ col ] ) {
-                        
-                    }
-                }
-            } 
-        }
-    }
-} */
