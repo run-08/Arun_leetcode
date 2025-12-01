e5758015a93440a62204d3eb5445786e5d2c0860
Arun T <137892849+run-08@users.noreply.github.com>
2025-12-01 21:58:37 +0530

    Time: 2 ms (99.15%), Space: 47.3 MB (6.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -18,3 +18,33 @@ class Solution {
        return new int[]{-1, -1};
     }
 }
+/*
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int rowSize = mat.length , colSize = mat [ 0 ].length;
+        int [] [] dp = new int [ rowSize ] [ colSize ];
+        for ( int [] arr : dp ) Arrays.fil( arr , Integer.MAX_VALUE - 1  );
+        for ( int index = 0 ; index < rowSize ; index++ ) {
+            for ( int index2 = 0 ; index2 < colSize ; index2++ ) {
+                if ( grid [ index ] [ index2 ] == 0 ) {
+                    dp [ index ] [ index1 ] = 0;
+                    continue;
+                }
+                Queue < int []  > q = new ArrayList <> ();
+                q.offer ( new int [] { index , index1 , 0 } );
+                while ( !q.isEmpty() ) {
+                    int [] adjacent = q.pol();
+                    int row = adjacent [ 0 ] , col = adjacent [ 1 ] , steps = adjacent [ 2 ];
+                    if ( dp [ index ] [ index1 ] != Integer.MAX_VALUE ) {
+                        dp [ index ] [ index1 ] = steps;
+                        break;
+                    }
+                    int minDistance = Integer.MAX_VALUE  selectedRow = row , selectedColumn = 
+                    if ( row + 1 < rowsize && minDistance >= dp [ row + 1 ] [ col ] ) {
+                        
+                    }
+                }
+            } 
+        }
+    }
+} */
