92df63212d14f67401140789c5078a0517871d7b
Arun T <137892849+run-08@users.noreply.github.com>
2025-12-24 09:57:34 +0530

    Time: 4 ms (18.13%), Space: 49.1 MB (8.88%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    Boolean[] dp;
+    int n;HashSet<String> set;
+    String s;
+    List<String> result;
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        this.n = s.length();
+        this.s = s;
+        this.result = new ArrayList<>();
+        dp = new Boolean[n];
+        set = new HashSet<>();
+        wordDict
+                .forEach(set::add);
+        helper(0,"");
+        return result;
+    }
+    private boolean helper(int idx,String cur){
+       if(idx==n){
+        result.add(cur.substring(0,cur.length()-1));
+        return true;
+       }
+       if(dp[idx] != null && !dp[idx]) return dp[idx]; 
+       dp[idx] = false; 
+       for(int i=idx+1;i<=n;i++) if(set.contains(s.substring(idx,i))) dp[idx] |= helper(i,cur+(s.substring(idx,i))+" ");
+       return dp[idx];
+    }
+}
