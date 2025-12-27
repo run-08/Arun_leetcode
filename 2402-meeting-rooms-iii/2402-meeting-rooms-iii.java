55ac7c261ffd60c83e9e42e53b27dcd392ddf249
Arun T <137892849+run-08@users.noreply.github.com>
2025-12-27 17:56:15 +0530

    Time: 78 ms (28.26%), Space: 137.3 MB (9.12%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        // declarion...
+        Arrays.sort(meetings,(a,b) -> a[0] == b[0] ? Integer.compare(a[1],b[1]):Integer.compare(a[0],b[0]));
+        PriorityQueue<RoomDetail> minHeap = new PriorityQueue<>((a,b) -> a.time != b.time ? Long.compare(a.time,b.time) : Long.compare(a.no,b.no));
+        PriorityQueue<Integer> unUsedRoom  = new PriorityQueue<>((a,b) -> a - b );
+        int[] roomUsed = new int[n];
+        int max = 0, room = 0;
+        // process...
+        Stream
+              .iterate( 0, roomNo -> roomNo < n , roomNo -> roomNo + 1)
+              .forEach( roomNo -> unUsedRoom.add(roomNo) );
+        for(int[] meeting: meetings){
+            while (!minHeap.isEmpty() && minHeap.peek().time <= meeting[0]) {
+                unUsedRoom.add(minHeap.poll().no);
+            }
+             if(unUsedRoom.isEmpty()){
+                RoomDetail finished = minHeap.poll();
+                int roomNo = finished.no;
+                long time =  finished.time+(meeting[1]-meeting[0]);
+                roomUsed[roomNo]++;
+                minHeap.add(new RoomDetail(roomNo, time));
+             }
+            //  room is available...
+             else {
+                int roomNo = unUsedRoom.poll();
+                roomUsed[roomNo]++;
+                minHeap.add(new RoomDetail(roomNo,meeting[1]) );
+             }
+        }
+        // Here, calculate the maximum used room....
+        for(int i=0;i<n;i++){
+            if(roomUsed[i] > max){
+                max = roomUsed[i];
+                room = i;
+            }
+        }
+        return room;
+    }
+}
+class RoomDetail{
+    int no;
+    long time;
+    RoomDetail(int no,long time){
+        this.no = no;
+        this.time = time;
+    }
+}
