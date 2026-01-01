bf1b8753f11b11785426d17288e6ca8309c51ba7
Arun T <137892849+run-08@users.noreply.github.com>
2026-01-01 18:25:13 +0530

    Time: 584 ms (6.93%), Space: 155.5 MB (5.94%) - LeetHub

---
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
@@ -0,0 +1,31 @@
+class Solution {
+    Map<String,Integer> memo;
+    public int minimumDistance(String word) {
+        memo = new HashMap<>();
+        return helper(-1,-1,word,0);
+    }
+    private int helper(int i,int j,String s,int idx){
+        if(idx==s.length()) return 0;
+        String key = idx+", "+i+", "+j;
+        if(memo.containsKey(key)) return memo.get(key);
+        // process 1st finger...
+        int x1 = (s.charAt(idx)-'A')/6,y1=(s.charAt(idx)-'A')%6;
+        int finger1 = 100000, finger2 = 100000;
+        if(i==-1){
+            finger1 = helper(idx,j,s,idx+1);
+        }
+        else{
+            int x2=(s.charAt(i)-'A')/6,y2=(s.charAt(i)-'A')%6;
+            finger1 = Math.abs(x1-x2)+Math.abs(y1-y2)+helper(idx,j,s,idx+1);
+        }
+        if(j==-1){
+           finger2 = helper(i,idx,s,idx+1);
+        }
+        else{
+           int x2=(s.charAt(j)-'A')/6,y2=(s.charAt(j)-'A')%6;
+           finger2 = Math.abs(x1-x2)+Math.abs(y1-y2)+(helper(i,idx,s,idx+1));
+        }
+        memo.put(key,Math.min(finger1,finger2));
+        return memo.get(key);
+    }
+}
