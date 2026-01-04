b1dcca60177de63952fcc553e1ecf0097a1d9178
Arun T <137892849+run-08@users.noreply.github.com>
2026-01-04 15:56:51 +0530

    Time: 44 ms (79.59%), Space: 45.1 MB (85.71%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -12,9 +12,22 @@ class Solution {
         if(dp[idx][changes][prev] != -1) return dp[idx][changes][prev];
         long count = 0;
         // Try out all possibilities...
-        for(int i=1;i<=m;i++) {
-            int max = Math.max(i,prev);
-            count = ( count + helper(max,n,(max>prev?1+changes:changes),k,idx+1,m) ) % MOD;
+        // for(int i=1;i<=m;i++) {
+        //     int max = Math.max(i,prev);
+        //     count = ( count + helper(max,n,(max>prev?1+changes:changes),k,idx+1,m) ) % MOD;
+        // }
+        // We need to consider only maximum, so just pickup the prev + 1 and prev - 1 and prev as possible...
+        // so only 3 states...
+        // Let us took prev - 1;
+        // if(prev-1 > 0)count = ( count + helper(prev-1,n,changes,k,idx+1,m))%MOD; 
+        // if(prev+1 >= m)count = ( count + helper(prev+1,n,changes+1,k,idx+1,m)) % MOD;
+        // count = ( count + helper(prev,n,changes,k,idx+1,m)) % MOD;
+        if (prev > 0) {
+            count += (long) prev *
+                     helper(prev, n, changes, k, idx + 1, m);
+        }
+        for (int newMax = prev + 1; newMax <= m; newMax++) {
+            count += helper(newMax, n, changes + 1, k, idx + 1, m);
         }
         return dp[idx][changes][prev] = (int)(count % MOD);
     }
